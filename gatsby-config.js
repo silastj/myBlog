9a93b8edb04fd8686ea6670783a14ee95bd68467
Amós Silas <silastj@hotmail.com>
2023-03-19 20:17:44 -0300

    cleanup starter stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,10 +9,10 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `My Blog`,
+    description: `A blog about frontend development and other cool stuff`,
+    author: `@asilas`,
+    siteUrl: `https://www.asilas.com.br`,
   },
   plugins: [
     `gatsby-plugin-image`,
